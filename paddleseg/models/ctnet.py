a6180140b5d6416a0ce59a776a3741f55cf759cd
xiaoxiao <39050296+syp2ysy@users.noreply.github.com>
2021-09-19 14:24:47 +0800

    Update ctnet.py

--- paddleseg/models/ctnet.py
+++ paddleseg/models/ctnet.py
@@ -191,7 +191,7 @@ class scm_layer(nn.Layer):
         self.key_channels = key_channels
 
         self.f_pixel = nn.Sequential(
-            layers.ConvBNReLU(in_channels, key_channels, 3, 1),   # or use "self.f_pixel = layers.ConvBNReLU(in_channels, key_channels, 3, 1)"
+            layers.ConvBNReLU(in_channels, key_channels, 3, 1),   # or use "self.f_pixel = layers.ConvBNReLU(in_channels, key_channels, 3, 1) or use self.f_pixel = layers.ConvBNReLU(in_channels, key_channels, 1)"
             layers.ConvBNReLU(key_channels, key_channels, 1))    # Not much difference in performance.
 	
 
